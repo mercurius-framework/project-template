5621266b26408263db9a90c57c370c512e16c4ba
Nictec <nicnac9911@gmail.com>
2023-08-16 15:27:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.INFO)
 app = MercuriusApplication({{ project_name }}, RootConfig, config_path="{{ project_name }}.toml", controllers_module="controllers")
 
 {% for module_name in selected_modules %}
-app.install(module_name)
+app.install({{module_name}})
 {% endfor %}
 
 # This if is important! Only this way the app can be started with an external application server
