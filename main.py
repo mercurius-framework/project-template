60ed2c0b7d6e6868ab57989c556bd276865eb6a0
Nictec <nicnac9911@gmail.com>
2023-08-16 12:09:56 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import logging
+from mercurius.core.application import MercuriusApplication
+
+from .config import RootConfig
+
+# change this as needed
+logging.basicConfig(level=logging.INFO)
+
+app = MercuriusApplication({{ project_name }}, RootConfig, config_path="{{ project_name }}.toml", controllers_module="controllers")
+
+{% for module_name in selected_modules %}
+app.install(module_name)
+{% endfor %}
+
+# This if is important! Only this way the app can be started with an external application server
+if __name__ == "__main__":
+    app.start()
